7590996ebe1690049164ef8472d02a60ada531fe
yortuc <yortucboylu@gmail.com>
2016-05-25 18:05:18 +0300

    basic require statements for method params

---
+++ CodeContractsExperiments/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace CodeContractsExperiments
+{
+    public class Person
+    {
+        private string name = "";
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public void Rename(string name) {
+            Contract.Requires(!string.IsNullOrWhiteSpace(name), "name must be a non-null, non-empty string");
+            this.name = name;
+        }
+
+        public void PrintPerson() {
+            string name = Char.ToUpper(this.name[0]) + this.name.Substring(1);
+
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ CodeContractsExperiments/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeContractsExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("evren");
+
+            person.PrintPerson();
+
+            person.Rename(null);
+
+            person.PrintPerson();
+
+            Console.ReadKey();
+        }
+    }
+}
